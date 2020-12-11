55f25fe4d4fec5c0675397d4c679f5f412262507
Ravi Ramcharan <rsramcharan@gmail.com>
2020-12-11 16:33:48 +0100

    Initial commit

---
+++ src/components/Translate.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+import Convert from "./Convert";
+
+const options = [
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+];
+//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="text">Enter text</label>
+          <input
+            id="text"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <Dropdown
+        options={options}
+        selected={language}
+        onSelectedChange={setLanguage}
+        label="Select a language"
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
+};
+
+export default Translate;
